95c107503e967500e825a20941e2be2e4b6e81a4
Dvitriy_Kolesnikov <dimas.kolesnikov123@gmail.com>
2020-03-14 16:58:18 +0200

    durac

---
+++ script.js
@@ -0,0 +1,38 @@
+﻿'use strict ';
+
+let hello = 'Hello world!';
+
+let message;
+
+// копируем значение 'Hello world' из переменной hello в переменную message
+message = hello;
+
+// теперь две переменные содержат одинаковые данные
+//alert(hello); // Hello world!
+//alert(message); // Hello world!
+
+let arr = ['rita', 'aac', 'adc'];
+
+//console.log( arr[0] );
+//alert('hello world');
+
+let answer = prompt("ifvbbdfvbhjbhvdj", "lff");
+
+//console.log( typeof(arr);
+
+
+let incr = 10,
+    decr = 10;
+
+
+    console.log(incr++);
+    console.log(--decr);
+
+let a = +prompt('aaa');
+    console.log( typeof(a) );
+
+let num = 50;
+
+(num == 50) ? console.log('верно') : console.log('НЕВЕРНО');
+    
+
