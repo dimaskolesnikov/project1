6734b53f9450b191f158abe53dea6628adc812a3
Dvitriy_Kolesnikov <dimas.kolesnikov123@gmail.com>
2020-03-16 18:41:51 +0200

    hhd

---
+++ homework1.js
@@ -0,0 +1,11 @@
+'use strict'
+
+let money = prompt('Ваш бюджет на месяц?', '');
+let time = prompt('Введите дату в формате YYYY-MM-DD','');
+
+let appData = {
+    budget: money,
+    timer: time,
+
+
+}
